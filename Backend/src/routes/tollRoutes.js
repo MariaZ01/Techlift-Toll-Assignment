2c4e30e544fb0624c9d8cbc456d0dd863c41e07b
MariaZ01 <mariaa.zaffar@gmail.com>
2023-03-16 02:12:14 +0500

    msg

--- Backend/src/routes/tollRoutes.js
+++ Backend/src/routes/tollRoutes.js
@@ -27,18 +27,19 @@ router.post('/exitpoint', async (req, res) => {
     // check is entry was made from this number plate
     const { distance, exitPoint, numberPlate } = req.body;
     const toll = await Toll.findOne({ numberPlate })
-    if (!toll) return res.status(404).send('yo')
+    if (!toll) return res.status(404).send('No entry was made')
 
     // constants
     const distancePerKmRate = 0.2;
     const baseRate = 20;
     //distance Cost = perkm rate * distance
     const distanceCost = distancePerKmRate * (distance);
-    
+    // weekends 
+    const isweekend = toll.day.toLowerCase() == "saturday" || toll.day.toLowerCase() == "sunday" ;
     //Base Cost
     const baseCost = distanceCost+ baseRate;
     // Final Cost
-    const FinalCost = baseCost * 1;
+    const FinalCost = baseCost * (isweekend ? 1.5 :1 );
 
     toll.distance = distance;
     toll.exitPoint = exitPoint;
@@ -52,6 +53,9 @@ router.post('/exitpoint', async (req, res) => {
 
 router.get("/getall", async (req, res) => {
   try {
+    // pagination 
+
+    // find all entries in toll
     const tolls = await Toll.find({});
     res.send(tolls);
   } catch (err) {
